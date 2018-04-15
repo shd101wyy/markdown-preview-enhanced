508ab70a972b596e4f3af57502fbdff22be27ac2
Yiyi Wang <shd101wyy@gmail.com>
2018-04-15 16:23:48 -0500

    Fix issue #836

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -307,11 +307,11 @@ Front matter is hidden by default, but you can choose between \`table\` (like [o
     title: "Drop image to editor",
     description: "What to do after you drop an image to editor",
     type: "string",
-    default: "upload",
+    default: "copy to image folder",
     enum: [
-      "upload",
       "insert relative path",
       "copy to image folder",
+      "upload",
       "do nothing",
     ],
     order: 77,
