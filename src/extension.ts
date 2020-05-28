8a2987dee0adf4052094a18f25618499c60fea9c
Yiyi Wang <shd101wyy@gmail.com>
2020-05-28 19:42:09 +0800

    feat: Upgraded mume

--- src/extension.ts
+++ src/extension.ts
@@ -319,38 +319,46 @@ export function activate(state) {
         "../../package.json",
       ))["version"];
       if (packageVersion !== mume.configs.config["atom_mpe_version"]) {
+        const mpeConfig = Object.assign({}, mume.configs.config, {
+          atom_mpe_version: packageVersion,
+        });
         fs.writeFileSync(
           path.resolve(mume.getExtensionConfigPath(), "config.json"),
-          JSON.stringify(
-            Object.assign({}, mume.configs.config, {
-              atom_mpe_version: packageVersion,
-            }),
-          ),
+          JSON.stringify(mpeConfig),
         );
-
-        const noty = atom.notifications.addInfo(
-          "If you like using markdown-preview-enhanced, please consider sponsoring the developer to help make this project better 😊.",
-          {
-            dismissable: true,
-            buttons: [
-              {
-                text: "Open GitHub Sponsors",
-                onDidClick: () => {
-                  mume.utility.openFile(
-                    "https://github.com/sponsors/shd101wyy",
-                  );
-                  noty.dismiss();
+        if (!mpeConfig["already_sponsored"]) {
+          const noty = atom.notifications.addInfo(
+            "If you like using markdown-preview-enhanced, please consider sponsoring the developer to help make this project better 😊.",
+            {
+              dismissable: true,
+              buttons: [
+                {
+                  text: "Open GitHub Sponsors",
+                  onDidClick: () => {
+                    mume.utility.openFile(
+                      "https://github.com/sponsors/shd101wyy",
+                    );
+                    noty.dismiss();
+                  },
                 },
-              },
-              {
-                text: "I already sponsored",
-                onDidClick: () => {
-                  noty.dismiss();
+                {
+                  text: "I already sponsored",
+                  onDidClick: () => {
+                    mpeConfig["already_sponsored"] = true;
+                    fs.writeFileSync(
+                      path.resolve(
+                        mume.getExtensionConfigPath(),
+                        "config.json",
+                      ),
+                      JSON.stringify(mpeConfig),
+                    );
+                    noty.dismiss();
+                  },
                 },
-              },
-            ],
-          },
-        );
+              ],
+            },
+          );
+        }
       }
     });
 }
